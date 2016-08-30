e47cb6abbc0e02963b6a5801b440f594c93ec7a9
1018225817@qq.com <1018225817@qq.com>
2016-08-30 22:03:39 +0800

    update

--- bugme.md
+++ bugme.md
@@ -3,7 +3,23 @@
 
 ## bug1
 
-`UnicodeDecodeError: 'gbk' codec can't decode byte 0xaf in position 18: illegal multibyte sequence`
+Q: `UnicodeDecodeError: 'gbk' codec can't decode byte 0xaf in position 18: illegal multibyte sequence`
 
-s：在打开是指定编码类型，如下：
+S：在打开是指定编码类型，如下：
 `f = open("foo.txt", "r", encoding='utf-8')`
+
+## bug2
+Q: 去除列表的重复元素
+
+S:
+```python
+l1 = ['b','c','d','b','c','a','a']
+l2 = list(set(l1))
+l2.sort(key=l1.index)
+print l2
+```
+
+## bug3
+Q:如何输出空格
+
+S:`\0`
--- kindle/demo.py
+++ kindle/demo.py
@@ -1,51 +1,69 @@
 import re
 BOUNDARY = u"==========\n"
+TWOBOUNDARY = u"==========\n==========\n"
 
 #主函数，目前可实现利用关键词进行简单的分割成列表。
-f = open("foo.txt", "r", encoding='utf-8')
+f = open("source.txt", "r", encoding='utf-8')
 content = f.read()  # 读取全部内容
+content = content.replace(u'\ufeff', u'') #替换书名前的空格
+#content = content.replace(TWOBOUNDARY,BOUNDARY)
 #print(content)
-numOfLine = content.find(BOUNDARY)
-print(numOfLine)
 clips = content.split(BOUNDARY)
-print(clips[0])
+print("列表个数：",clips.__len__()) # 获取列表的个数
+for i in range(0,4):
+    print(clips[i])
+sum = clips.__len__()
+print(sum)
+
+# 获取书名存储为列表books，获取除书名外的内容为sentence
+books = []
+sentence = []
+for i in range(0,sum):
+    book = clips[i].split("\n-")
+    print(book)
+    if (book!=['']): # 如果书名为空
+        books.append(book[0])
+        sentence.append(book[1])
+#print(sentence)
+print(books.__len__())
+print(sentence.__len__())
+
+# 处理sentence列表
+time = [] #未处理
+date = [] #处理1
+word = [] #处理2
+mark = [] #结果
+for i in range(0,sentence.__len__()):
+    time = sentence[i].split("于 ")
+    print(time[1])
+    date.append(time[1])
+    for j in range(0,date.__len__()):
+         word = date[j].split("\n\n")
+         if (word!=['']):
+            print(word[0])
+            mark.append(word[0])
+print(mark)
+print(mark.__len__())
+#print(word)
+#print(word.__len__())
 
 
-#for i in (2,numOfLine):
-# if numOfLine>0:
-#       print(content)
 
-# for line in lines:
-#     print(line)
+## 去除书名列表中的重复元素
+# nameOfBooks = list(set(books))
+# nameOfBooks.sort(key=books.index)
+# print(nameOfBooks)
+# print(nameOfBooks.__len__()) # 书名的总数
+# #print(nameOfBooks.count(""))
+#
+# # 根据不同书名建立列表
+# books_dict = {}
+# for j in range(0,nameOfBooks.__len__()):
+#     books_dict [nameOfBooks[j]] = j
+# print("\n字典长度：",len(books_dict))
+# print(books_dict)
 
-# BOUNDARY = u"==========\r\n"
-#
-# def get_sections(filename):
-#     with open(filename, 'r',encoding='utf-8') as f:
-#         content = f.read()
-#     content = content.replace(u'\ufeff', u'')
-#     return content.split(BOUNDARY)
-#
-# def get_clip(section):
-#     clip = {}
-#
-#     lines = [l for l in section.split(u'\r\n') if l]
-#     if len(lines) != 3:
-#         return
-#
-#     clip['book'] = lines[0]
-#     match = re.search(r'(\d+)-\d+', lines[1])
-#     if not match:
-#         return
-#     position = match.group(1)
-#
-#     clip['position'] = int(position)
-#     clip['content'] = lines[2]
-#
-#     return clip
-#
-# sections=get_sections("foo.txt")
-# for section in sections:
-#     print(section)
-#     print("================================")
-#
+
+
+
+
